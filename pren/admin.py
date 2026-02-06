50fc624c06d1222e063fb3cc86ee2cf372e74c1c
Semen Ulnyrov <semen.ulnyrov12@gmail.com>
2026-02-06 10:49:52 +0100

    aggiunti modelli piscina, media e admin funzionante

--- pren/admin.py
+++ pren/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Piscina, ImmaginePiscina, Sdraio
 
-# Register your models here.
+@admin.register(Piscina)
+class PiscinaAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome")
+
+
+@admin.register(ImmaginePiscina)
+class ImmaginePiscinaAdmin(admin.ModelAdmin):
+    list_display = ("id", "piscina", "caricata_il")
+
+
+@admin.register(Sdraio)
+class SdraioAdmin(admin.ModelAdmin):
+    list_display = ("id", "piscina", "etichetta", "x_percentuale", "y_percentuale")
+    list_filter = ("piscina",)
+    search_fields = ("etichetta",)
+
--- pren/models.py
+++ pren/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Piscina(models.Model):
+    nome = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.nome
+
+class ImmaginePiscina(models.Model):
+    piscina = models.OneToOneField(
+        Piscina,
+        on_delete=models.CASCADE,
+        related_name="immagine"
+    )
+
+    immagine = models.ImageField(upload_to="immagini_piscine/")
+    
+    caricata_il = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Immagine - {self.piscina.nome}"
+    
+
+class Sdraio(models.Model):
+    piscina = models.ForeignKey(
+        Piscina,
+        on_delete=models.CASCADE,
+        related_name="sdrai"
+    )
+
+    etichetta = models.CharField(max_length=50, blank=True, default="")
+
+    x_percentuale = models.FloatField(
+        validators=[MinValueValidator(0.0), MaxValueValidator(100.0)],
+        default=0.0
+    )
+
+    y_percentuale = models.FloatField(
+        validators=[MinValueValidator(0.0), MaxValueValidator(100.0)],
+        default=0.0
+    )
+
+    def __str__(self):
+        return f"{self.piscina.nome} - {self.etichetta or f'Sdraio #{self.pk}'}"
+    
